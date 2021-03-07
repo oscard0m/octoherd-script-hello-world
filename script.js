81b170660d2a1efe86cd57d90ef15f37021d7951
Oscar Dominguez <dominguez.celada@gmail.com>
2021-03-07 15:31:55 +0100

    feat: initial version

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ *
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
